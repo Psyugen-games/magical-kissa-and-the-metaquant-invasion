19375599b019495ab210328fc8a67f53199c8104
Laura Mazzuca <lauramazzuca@protonmail.com>
2021-11-25 10:36:34 +0100

    refactors folders

--- Magical Girl v1/Assets/Scripts/CharacterController2D.cs
+++ Magical Girl v1/Assets/Scripts/CharacterController2D.cs
@@ -133,7 +133,7 @@ public class CharacterController2D : MonoBehaviour
 
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
+        theScale.x *= -1;
 		transform.localScale = theScale;
 	}
 
